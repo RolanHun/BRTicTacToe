294cdd4c8f53689cefd1e1ab1fa56af392d42081
RolanHun <kisbartok01@gmail.com>
2021-05-26 01:26:33 +0200

    TicTacToe Final

--- public_html/JSWeb.js
+++ public_html/JSWeb.js
@@ -5,7 +5,6 @@ $(function(){
 var meret=3;
 var XO=1;
 var GameIsOn=0;
-var kepek=[startPic,xPic,oPic];
 var startPic={
     eleresiUt:"pics/start.png",
     alt:"start"
@@ -18,18 +17,75 @@ var oPic={
     eleresiUt:"pics/oPic.png",
     alt:"o"
 };
-var winGif={
-    eleresiUt:"pics/win.gif",
+var XwinGif={
+    eleresiUt:"pics/winX.gif",
     alt:"Winner"
 };
+var OwinGif={
+    eleresiUt:"pics/winO.gif",
+    alt:"Winner"
+};
+var tieGif={
+    eleresiUt:"pics/tie.gif",
+    alt:"Tie"
+};
+var kezdoSzin=1;
 var jeloltek=[];
 function NewGameKlikk() {
+        var kezdoJatekos=Math.floor(Math.random() * 2);
+        var XColor = document.getElementById("XSelect");
+        if (kezdoSzin===1){
+                if (XColor.options[XColor.selectedIndex].text==="Fekete"){
+                    xPic={
+                        eleresiUt:"pics/xPic.png",
+                        alt:"x"
+                    };
+                }
+                else if (XColor.options[XColor.selectedIndex].text==="Fehér"){
+                    xPic={
+                        eleresiUt:"pics/xPicF.png",
+                        alt:"x"
+                    };
+                }
+                else if (XColor.options[XColor.selectedIndex].text==="Zöld"){
+                    xPic={
+                        eleresiUt:"pics/xPicZ.png",
+                        alt:"x"
+                    };
+                }
+                var OColor = document.getElementById("OSelect");
+                if (OColor.options[OColor.selectedIndex].text==="Fekete"){
+                    oPic={
+                        eleresiUt:"pics/oPic.png",
+                        alt:"o"
+                    };
+                }
+                else if (OColor.options[OColor.selectedIndex].text==="Fehér"){
+                    oPic={
+                        eleresiUt:"pics/oPicF.png",
+                        alt:"o"
+                    };
+                }
+                else if (OColor.options[OColor.selectedIndex].text==="Zöld"){
+                    oPic={
+                        eleresiUt:"pics/oPicZ.png",
+                        alt:"o"
+                    };
+                }
+                kezdoSzin=0;
+        }
         $("#GameChanger").empty();
         $("#GameField").empty();
+        if (kezdoJatekos===1){
+            XO=1;
+            $("#JelenFeedBack").html("<br> Az X kezd! ");
+        }
+        else {
+            XO=0;
+            $("#JelenFeedBack").html("<br> Az O kezd! ");
+        }
         GameIsOn=1;
         jeloltek=["0","1","2","3","4","5","6","7","8"];
-        XO=1;
-        $("#JelenFeedBack").html("<br> Az X kezd! ");
         $("#MainFeedBack").html("Sok sikert! (3x3-as mező)");
         for (var i = 0; i < meret * meret; i++) {
             $("#GameField").append("<img>");
@@ -48,13 +104,20 @@ function kattintasra() {
             $(this).attr("src",xPic.eleresiUt);
             $(this).attr("alt",xPic.alt);
             XO=0;
-            if(jeloltek[0]==="X" && jeloltek[1]==="X" && jeloltek[2]==="X" || jeloltek[3]==="X" && jeloltek[4]==="X" && jeloltek[5]==="X" || jeloltek[6]==="X" && jeloltek[7]==="X" && jeloltek[8]==="X" || jeloltek[0]==="X" && jeloltek[3]==="X" && jeloltek[6]==="X" || jeloltek[1]==="X" && jeloltek[4]==="X" && jeloltek[7]==="X" || jeloltek[2]==="X" && jeloltek[5]==="X" && jeloltek[8]==="X" || jeloltek[0]==="X" && jeloltek[4]==="X" && jeloltek[8]==="X" || jeloltek[2]==="X" && jeloltek[4]==="X" && jeloltek[6]==="X"){
+            if (jeloltek[0]==="X" && jeloltek[1]==="X" && jeloltek[2]==="X" || jeloltek[3]==="X" && jeloltek[4]==="X" && jeloltek[5]==="X" || jeloltek[6]==="X" && jeloltek[7]==="X" && jeloltek[8]==="X" || jeloltek[0]==="X" && jeloltek[3]==="X" && jeloltek[6]==="X" || jeloltek[1]==="X" && jeloltek[4]==="X" && jeloltek[7]==="X" || jeloltek[2]==="X" && jeloltek[5]==="X" && jeloltek[8]==="X" || jeloltek[0]==="X" && jeloltek[4]==="X" && jeloltek[8]==="X" || jeloltek[2]==="X" && jeloltek[4]==="X" && jeloltek[6]==="X"){
                 $("#MainFeedBack").html("<br>  X Nyerte a játékot! Ha szeretnél játszani még kattints az új játék gombra!");
                 $("#JelenFeedBack").html("<img>");
-                    $("#JelenFeedBack img").attr("src",winGif.eleresiUt);
-                    $("#JelenFeedBack img").attr("alt",winGif.alt);
+                    $("#JelenFeedBack img").attr("src",XwinGif.eleresiUt);
+                    $("#JelenFeedBack img").attr("alt",XwinGif.alt);
                 winnerXO=1;
                 GameIsOn=0;
+            }
+            else if(jeloltek[0]!=="0" && jeloltek[1]!=="1" && jeloltek[2]!=="2" && jeloltek[3]!=="3" && jeloltek[4]!=="4" && jeloltek[5]!=="5" && jeloltek[6]!=="6" && jeloltek[7]!=="7" && jeloltek[8]!=="8"){
+                GameIsOn=0;
+                $("#MainFeedBack").html("<br>  Döntetlen! Ha szeretnél játszani még kattints az új játék gombra!");
+                $("#JelenFeedBack").html("<img>");
+                    $("#JelenFeedBack img").attr("src",tieGif.eleresiUt);
+                    $("#JelenFeedBack img").attr("alt",tieGif.alt);
             }
             else{
             $("#JelenFeedBack").html("<br>  O !");
@@ -69,10 +132,17 @@ function kattintasra() {
             if(jeloltek[0]==="O" && jeloltek[1]==="O" && jeloltek[2]==="O" || jeloltek[3]==="O" && jeloltek[4]==="O" && jeloltek[5]==="O" || jeloltek[6]==="O" && jeloltek[7]==="O" && jeloltek[8]==="O" || jeloltek[0]==="O" && jeloltek[3]==="O" && jeloltek[6]==="O" || jeloltek[1]==="O" && jeloltek[4]==="O" && jeloltek[7]==="O" || jeloltek[2]==="O" && jeloltek[5]==="O" && jeloltek[8]==="O" || jeloltek[0]==="O" && jeloltek[4]==="O" && jeloltek[8]==="O" || jeloltek[2]==="O" && jeloltek[4]==="O" && jeloltek[6]==="O"){
                 $("#MainFeedBack").html("<br>  O Nyerte a játékot! Ha szeretnél játszani még kattints az új játék gombra!");
                 $("#JelenFeedBack").html("<img>");
-                    $("#JelenFeedBack img").attr("src",winGif.eleresiUt);
-                    $("#JelenFeedBack img").attr("alt",winGif.alt);
+                    $("#JelenFeedBack img").attr("src",OwinGif.eleresiUt);
+                    $("#JelenFeedBack img").attr("alt",OwinGif.alt);
                 winnerXO=2;
                 GameIsOn=0;
+            }
+            else if(jeloltek[0]!=="0" && jeloltek[1]!=="1" && jeloltek[2]!=="2" && jeloltek[3]!=="3" && jeloltek[4]!=="4" && jeloltek[5]!=="5" && jeloltek[6]!=="6" && jeloltek[7]!=="7" && jeloltek[8]!=="8"){
+                GameIsOn=0;
+                $("#MainFeedBack").html("<br>  Döntetlen! Ha szeretnél játszani még kattints az új játék gombra!");
+                $("#JelenFeedBack").html("<img>");
+                    $("#JelenFeedBack img").attr("src",tieGif.eleresiUt);
+                    $("#JelenFeedBack img").attr("alt",tieGif.alt);
             }
             else{
             $("#JelenFeedBack").html("<br>  X !");
